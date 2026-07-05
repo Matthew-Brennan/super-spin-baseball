fe27e18aad3f54ac2970dfd5b9ad8d1ac9602c27
Matthew-Brennan <matthew.brennan4@hotmail.com>
2026-07-04 23:34:13 -0400

    added some comments for tomorrow

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 # Ball and chain variables
 @export var ball_and_chain: PackedScene
+@export var top_speed: float
 
 var currentBnC: Sprite2D
 
@@ -20,3 +21,14 @@ func _physics_process(delta):
 	
 	# Position the ball and chain in the direction of the mouse
 	currentBnC.global_position = global_position + direction * currentBnC.chain_length
+
+
+
+
+
+func throw():
+	#torque is r x F. r is chain length and f is mass x acceleration(which will kinda just be speed for our purposes)
+	# mass and chain length a stats on the ball and chain, top speed comes from player stats and is determinded by how fast they spin
+	#we're going to cheat a little and only throw foward
+	# maybe add a bonus if the player actually does throw forward and alert them with a graphic that says perfect throw 
+	pass
